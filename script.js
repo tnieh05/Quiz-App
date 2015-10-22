fc69bb97c4b54384d52d491f3f463cc817cfb62e
tnieh05 <tnieh05@hotmail.com>
2015-10-22 14:13:48 -0700

    new layout

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+
+	var answers = ["b", "a", "a", "c", "d", "d"];
+	var userAnswers = {};
+	var counter = 0;
+	var classHighlight = "hightlight"
+	var currentQ = 0
 	
 		$(".clouds").effect("bounce", {times: 4}, 250);
 	
@@ -13,6 +19,7 @@ $(document).ready(function(){
 		$(this).parents(".question-container").fadeOut(500, function(){
 			$(this).next().fadeIn(500);
 			$(".plane-img").animate({left: "+=13.66%"})
+			currentQ++
 
 		});
 	})
@@ -21,23 +28,41 @@ $(document).ready(function(){
 		$(this).parents(".question-container").fadeOut(500, function(){
 			$(this).prev().fadeIn(500);
 			$(".plane-img").animate({left: "-=13.66%"})
+			currentQ--
 		});
 	})
+
+
+
+	$(".answers li").on("click", "a", function(){
+		var usersAnswr = $(this).data("key");
+			userAnswers[currentQ] = usersAnswr 
+			$(".answers li a").removeClass("highlight");
+			$(this).addClass("highlight"); 
+
+
+
+			console.log(userAnswers);
+			
+	})
+
+
 
 	$(".showResult").click(function(){
 		$(this).parents(".question-container").fadeOut(500, function(){
-
+			$(this).next().fadeIn(500);
 		});
+
+		for(var i = 0; i < answers.length; i++){
+			if(answers[i] == userAnswers[i]){
+					counter++;
+					
+			}
+		}
+		$(".answrs-correct").html(counter + " out of " + answers.length + " correct!")
+		
 	})
 
-
-	var correct = { 
-		answers: ["b", "d", "d", "c", "d", "b"]
-	}
-
-	var userAnswers = [];
-
-
-
+	
 
 })
